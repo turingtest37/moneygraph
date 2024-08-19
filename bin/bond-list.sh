b2762d16f633cbc306c6ab9ab7ba3174aeac1e3d
dbeeson <c.difficile@gmail.com>
2024-08-19 16:38:17 -0400

    New bond listing features

--- bin/bond-list.sh
+++ bin/bond-list.sh
@@ -1,18 +1,23 @@
 #! /bin/sh
+
 FILE=`realpath $1`
+echo "Processing ${FILE}..."
 
 PROJBASE=/Users/doug/dev/questrade
 SERVER=http://127.0.0.1:7200/repositories/Investments/statements
-NAMEBASE=valuation
+NAMEBASE=bond-list
 NAMED_GRAPH_BASE="https://triples.beesondouglas.com/questrade/__${NAMEBASE}__"
 QUERY=$PROJBASE/src/$NAMEBASE.rq
 OUT=$PROJBASE/out/$NAMEBASE.nt
 UPLOAD=$PROJBASE/out/$NAMEBASE-1.nt
-CURRENT_GRAPH="${NAMED_GRAPH_BASE}latest"
+CURRENT_GRAPH="${NAMED_GRAPH_BASE}"
+echo "CURRENT_GRAPH=$CURRENT_GRAPH"
+# Extract date from $FILE using sed or awk; use that date for ARCHIVE_GRAPH
 FILEDATE=`basename $FILE | sed 's/[^0-9]*$//' | sed 's/^[^0-9]*//'`
-ARCHIVE_GRAPH="${NAMED_GRAPH_BASE}archive"
 echo "FILEDATE = $FILEDATE"
+ARCHIVE_GRAPH="${NAMED_GRAPH_BASE}archive"
 echo "ARCHIVE_GRAPH = $ARCHIVE_GRAPH"
+# ARCHIVE_GRAPH="${NAMED_GRAPH_BASE}`date -I`"
 OUTOPT="-o ${OUT}"
 FORMAT="NT"
 
@@ -24,20 +29,18 @@ then
     OUTOPT=""
 fi
 
-echo "Processing ${FILE} with ${QUERY}..."
 java -jar ~/dev/sparql-anything-0.8.2.jar -v -q "${QUERY}" -f ${FORMAT} ${OUTOPT} -v loc="${FILE}" -v dt="${FILEDATE}"
 
 if test "x$2" == "x"
 then 
-# echo $NAMED_GRAPH
 echo "Uploading to ${ARCHIVE_GRAPH}..."
-curl -i -v -H "Content-Type: application/n-triples" --data-binary @"$UPLOAD" --url-query "context=<${ARCHIVE_GRAPH}>" "$SERVER"
+curl -i -H "Content-Type: application/n-triples" --data-binary @"$UPLOAD" --url-query "context=<${ARCHIVE_GRAPH}>" "$SERVER"
 
 echo "Clearing ${CURRENT_GRAPH}..."
-curl -i -v -H "Content-Type: application/sparql-update" --data-binary "CLEAR GRAPH <${CURRENT_GRAPH}>" "$SERVER"
+curl -i -H "Content-Type: application/sparql-update" --data-binary "CLEAR GRAPH <${CURRENT_GRAPH}>" "$SERVER"
 
 echo "Uploading to ${CURRENT_GRAPH}..."
-curl -i -v -H "Content-Type: application/n-triples" --data-binary @"$UPLOAD" --url-query "context=<$CURRENT_GRAPH>" "$SERVER"
+curl -i -H "Content-Type: application/n-triples" --data-binary @"$UPLOAD" --url-query "context=<$CURRENT_GRAPH>" "$SERVER"
 fi
 
 echo "Done."
