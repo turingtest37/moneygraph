072ff90a0444419786cfcd5741e0297c560f1494
dbeeson <c.difficile@gmail.com>
2024-08-25 11:33:11 -0400

    Fixed paths, remove old QT stuff

--- bin/ontologies.sh
+++ bin/ontologies.sh
@@ -5,15 +5,15 @@ SERVER=http://127.0.0.1:7200/repositories/Investments/statements
 NAMED_GRAPH_BASE=https://w3id.org/moneygraph/ns/data/__ontology
 
 GIST_GRAPH="${NAMED_GRAPH_BASE}__gist"
-QT_GRAPH="${NAMED_GRAPH_BASE}__qt"
+QT_GRAPH="${NAMED_GRAPH_BASE}__mg"
 # echo $NAMED_GRAPH
-QT_ONTOLOGY="${PROJBASE}/ontologies/questrade.ttl"
+QT_ONTOLOGY="${PROJBASE}/ontologies/moneygraph.ttl"
 
 echo "Clearing gist ontology NG..."
 curl -i -v -H "Content-Type: application/sparql-update" --data-binary "CLEAR GRAPH <${GIST_GRAPH}>" "$SERVER"
 
 echo "Uploading gist ontology files..."
-for UPLOAD in ${PROJBASE}/ontologies/gist*; do
+for UPLOAD in ${PROJBASE}/ontologies/gist*.ttl; do
 echo "Uploading ${UPLOAD}..."
     curl -i -v -H "Content-Type: text/turtle" --data-binary @"$UPLOAD" --url-query "context=<${GIST_GRAPH}>" "$SERVER"
 done
