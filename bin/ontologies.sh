62d8fc978a025f306a8d980ca18b6bdf7f5ec97f
dbeeson <c.difficile@gmail.com>
2024-08-31 17:46:38 -0400

    Uses onto_tool to prepare ontologies for export.

--- bin/ontologies.sh
+++ bin/ontologies.sh
@@ -5,9 +5,13 @@ SERVER=http://127.0.0.1:7200/repositories/Investments/statements
 NAMED_GRAPH_BASE=https://w3id.org/moneygraph/ns/data/__ontology
 
 GIST_GRAPH="${NAMED_GRAPH_BASE}__gist"
-QT_GRAPH="${NAMED_GRAPH_BASE}__mg"
+MG_GRAPH="${NAMED_GRAPH_BASE}__mg"
 # echo $NAMED_GRAPH
-QT_ONTOLOGY="${PROJBASE}/ontologies/moneygraph.ttl"
+MG_ONTOLOGY="${PROJBASE}/ontologies/moneygraph.ttl"
+MG_ONTOLOGY_EXPORT="${PROJBASE}/out/moneygraph_export.ttl"
+
+echo "Getting moneygraph ready for export..."
+onto_tool export -f turtle -o "${MG_ONTOLOGY_EXPORT}" -b strict "${MG_ONTOLOGY}"
 
 echo "Clearing gist ontology NG..."
 curl -i -v -H "Content-Type: application/sparql-update" --data-binary "CLEAR GRAPH <${GIST_GRAPH}>" "$SERVER"
@@ -18,9 +22,9 @@ echo "Uploading ${UPLOAD}..."
     curl -i -v -H "Content-Type: text/turtle" --data-binary @"$UPLOAD" --url-query "context=<${GIST_GRAPH}>" "$SERVER"
 done
 
-echo "Clearing Questrade ontology NG..."
-curl -i -v -H "Content-Type: application/sparql-update" --data-binary "CLEAR GRAPH <${QT_GRAPH}>" "$SERVER"
+echo "Clearing Moneygraph ontology NG..."
+curl -i -v -H "Content-Type: application/sparql-update" --data-binary "CLEAR GRAPH <${MG_GRAPH}>" "$SERVER"
 
-echo "Uploading Questrade ontology ${QT_ONTOLOGY}..."
-curl -i -v -H "Content-Type: text/turtle" --data-binary @"${QT_ONTOLOGY}" --url-query "context=<$QT_GRAPH>" "$SERVER"
+echo "Uploading Moneygraph ontology ${MG_ONTOLOGY}..."
+curl -i -v -H "Content-Type: text/turtle" --data-binary @"${MG_ONTOLOGY_EXPORT}" --url-query "context=<$MG_GRAPH>" "$SERVER"
 
